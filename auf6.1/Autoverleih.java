ff65ea86b3ad6d09114f86aa20989fdca19587e5
Kai <kairaden@web.de>
2025-10-22 10:59:52 +0200

    create auf6.1 directory and autoverleih files

---
+++ auf6.1/Autoverleih.java
@@ -0,0 +1,27 @@
+// loe06_klasse
+// auf6.1
+
+public class Autoverleih
+{
+   public static void main(String[] args)
+   {
+      Auto auto1 = new Auto();
+      Auto auto2 = new Auto();
+      Auto auto3 = new Auto();
+
+      auto1.autonummer = "HH-KR";
+      auto2.autonummer = "HH-PH";
+      auto3.autonummer = "HH-MS";
+      
+      auto1.tagespreis = 50;
+      auto2.tagespreis = 75;
+      auto3.tagespreis = 100;
+
+      auto1.farbe = Farbe.schwarz;
+      auto2.farbe = Farbe.rot;
+      auto3.farbe = Farbe.blau;
+
+      // Die folgende Zeile darf nicht veraendert werden:
+      Darsteller.zeige(auto1, auto2, auto3);
+   }
+}
